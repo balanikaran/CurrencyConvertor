a8b4c1d93a648b327306175128e39ae8d745450a
Karan Balani <karankbalani@gmail.com>
2021-03-18 00:40:13 +0530

    added code for scrollview and opening urls

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,37 +1,45 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { Alert, Linking, SafeAreaView, ScrollView } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 
 import colors from "../constants/colors";
 import RowItem from "../components/RowItem";
 import RowSeparator from "../components/RowSeparator";
 
+const openUrl = (url) => {
+  return Linking.openURL(url).catch((err) => {
+    Alert.alert("Sorry! Something went wrong.", `${err}`);
+  });
+};
+
 export default () => {
   return (
-    <SafeAreaView>
-      <RowItem
-        text="Themes"
-        onPress={() => alert("todo! :)")}
-        rightIcon={
-          <Entypo name="chevron-right" size={20} color={colors.blue} />
-        }
-      />
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView>
+        <RowItem
+          text="Themes"
+          onPress={() => openUrl("https://krnblni.github.io")}
+          rightIcon={
+            <Entypo name="chevron-right" size={20} color={colors.blue} />
+          }
+        />
 
-      <RowSeparator />
+        <RowSeparator />
 
-      <RowItem
-        text="React Native Basics"
-        onPress={() => alert("todo! :)")}
-        rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
-      />
+        <RowItem
+          text="React Native Basics"
+          onPress={() => openUrl("httpst://krnblni.github.io")}
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+        />
 
-      <RowSeparator />
+        <RowSeparator />
 
-      <RowItem
-        text="React Native by Example"
-        onPress={() => alert("todo! :)")}
-        rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
-      />
+        <RowItem
+          text="React Native by Example"
+          onPress={() => openUrl("https://krnblni.github.io")}
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 };
